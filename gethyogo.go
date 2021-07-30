d824f789283622bc33cb6366cc487c3f65ac322a
mitsugu oyama <mitsugu.oyama@gmail.com>
2021-07-30 18:34:48 +0900

    first commit

---
+++ gethyogo.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+  "fmt"
+  "strings"
+  "time"
+  "strconv"
+  "github.com/antchfx/htmlquery"
+)
+
+func main() {
+  url := "http://web.pref.hyogo.lg.jp/kf16/coronavirus_data.html"
+  doc, _ := htmlquery.LoadURL(url)
+
+  // 年月日
+  year := time.Now().Year()
+  path := "//*[@id='kensa_new']/following-sibling::text()"
+  node, _ := htmlquery.Query(doc, path)
+  d := strings.Replace(strings.Replace(strings.Replace(node.Data, "月", "/", 1), "日24時現在）", "", 1), "（", strconv.Itoa(year)+"/", 1)
+
+
+  // 宿泊療養
+  path = "/html/body/div[2]/div[4]/div/div[2]/div/div[2]/table[2]/tbody/tr[4]/td[6]/text()"
+  node, _ = htmlquery.Query(doc, path)
+  syuku := strings.Replace(node.Data, ",", "", 1)
+  // 軽症・中等症
+  path = "/html/body/div[2]/div[4]/div/div[2]/div/div[2]/table[2]/tbody/tr[4]/td[4]/text()"
+  node, _ = htmlquery.Query(doc, path)
+  kei := strings.Replace(node.Data, ",", "", 1)
+  // 入院・宿泊療養調整等
+  path = "/html/body/div[2]/div[4]/div/div[2]/div/div[2]/table[2]/tbody/tr[4]/td[7]/text()"
+  node, _ = htmlquery.Query(doc, path)
+  cyou := strings.Replace(node.Data, ",", "", 1)
+  // 自宅療養
+  path = "/html/body/div[2]/div[4]/div/div[2]/div/div[2]/table[2]/tbody/tr[4]/td[9]/text()"
+  node, _ = htmlquery.Query(doc, path)
+  home := strings.Replace(node.Data, ",", "", 1)
+  // その他医療機関福祉施設等
+  path = "/html/body/div[2]/div[4]/div/div[2]/div/div[2]/table[2]/tbody/tr[4]/td[10]/text()"
+  node, _ = htmlquery.Query(doc, path)
+  fukushi := strings.Replace(node.Data, ",", "", 1)
+  // 重症
+  path = "/html/body/div[2]/div[4]/div/div[2]/div/div[2]/table[2]/tbody/tr[4]/td[5]/text()"
+  node, _ = htmlquery.Query(doc, path)
+  jyuu := strings.Replace(node.Data, ",", "", 1)
+  // 死亡
+  path = "/html/body/div[2]/div[4]/div/div[2]/div/div[2]/table[2]/tbody/tr[4]/td[11]/text()"
+  node, _ = htmlquery.Query(doc, path)
+  shibou := strings.Replace(node.Data, ",", "", 1)
+  // 退院
+  path = "/html/body/div[2]/div[4]/div/div[2]/div/div[2]/table[2]/tbody/tr[4]/td[12]/text()"
+  node, _ = htmlquery.Query(doc, path)
+  taiin := strings.Replace(node.Data, ",", "", 1)
+
+  fmt.Printf("%v, %v, %v, %v, %v, %v, %v,, %v, %v\n", d, syuku, kei, cyou, home, fukushi, jyuu, shibou, taiin)
+
+}
+
+  /*
+  path = ""
+  node, _ = htmlquery.Query(doc, path)
+   := node.Data
+   */
+
+  /*
+
+  // 年月日
+  d, _ := jsonpointer.Get(positives, "/last_update")
+
+  // 軽症・中等症
+  kei, _ := jsonpointer.Get(positives,
+                            "/children/0/children/0/children/0/value")
+  // 重症
+  jyuu, _ := jsonpointer.Get(positives,
+                              "/children/0/children/0/children/1/value")
+  // 宿泊療養
+  syuku, _ := jsonpointer.Get(positives, "/children/0/children/1/value")
+  // 入院・宿泊療養調整等
+  cyou, _ := jsonpointer.Get(positives, "/children/0/children/2/value")
+  // 自宅療養
+  home, _ := jsonpointer.Get(positives, "/children/0/children/3/value")
+  // その他医療機関福祉施設等
+  fukushi, _ := jsonpointer.Get(positives, "/children/0/children/4/value")
+  // 死亡
+  shibou, _ := jsonpointer.Get(positives, "/children/0/children/5/value")
+  // 退院
+  taiin, _ := jsonpointer.Get(positives, "/children/0/children/6/value")
+
+  fmt.Printf("%v, %v, %v, %v, %v, %v, %v,, %v, %v\n", d, syuku, kei, cyou, home, fukushi, jyuu, shibou, taiin)
+  */
+
---
+++ go.mod
@@ -0,0 +1,5 @@
+module gethyogo
+
+go 1.16
+
+require github.com/antchfx/htmlquery v1.2.3
