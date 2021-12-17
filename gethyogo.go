a3af093892994aa8fd9033838931038bc4d9fb1b
mitsugu oyama <mitsugu.oyama@gmail.com>
2021-12-17 20:40:44 +0900

    It was made to follow the change of the page structure.

--- gethyogo.go
+++ gethyogo.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	url := "http://web.pref.hyogo.lg.jp/kf16/coronavirus_data.html"
+  url := "https://web.pref.hyogo.lg.jp/kf16/coronavirus_data.html"
 	doc, _ := htmlquery.LoadURL(url)
 
 	// 年月日
@@ -19,35 +19,35 @@ func main() {
 	d := strings.Replace(strings.Replace(strings.Replace(node.Data, "月", "/", 1), "日24時現在）", "", 1), "（", strconv.Itoa(year)+"/", 1)
 
 	// 宿泊療養
-	path = "/html/body/div[2]/div[4]/div/div[2]/div/div[2]/table[2]/tbody/tr[4]/td[6]/text()"
+  path = "//div[@id='tmp_contents']/table[2]/tbody/tr[4]/td[6]/text()"
 	node, _ = htmlquery.Query(doc, path)
 	syuku := strings.Replace(node.Data, ",", "", 1)
 	// 軽症・中等症
-	path = "/html/body/div[2]/div[4]/div/div[2]/div/div[2]/table[2]/tbody/tr[4]/td[4]/text()"
+  path = "//div[@id='tmp_contents']/table[2]/tbody/tr[4]/td[4]/text()"
 	node, _ = htmlquery.Query(doc, path)
 	kei := strings.Replace(node.Data, ",", "", 1)
 	// 入院・宿泊療養調整等
-	path = "/html/body/div[2]/div[4]/div/div[2]/div/div[2]/table[2]/tbody/tr[4]/td[7]/text()"
+  path = "//div[@id='tmp_contents']/table[2]/tbody/tr[4]/td[7]/text()"
 	node, _ = htmlquery.Query(doc, path)
 	cyou := strings.Replace(node.Data, ",", "", 1)
 	// 自宅療養
-	path = "/html/body/div[2]/div[4]/div/div[2]/div/div[2]/table[2]/tbody/tr[4]/td[9]/text()"
+  path = "//div[@id='tmp_contents']/table[2]/tbody/tr[4]/td[9]/text()"
 	node, _ = htmlquery.Query(doc, path)
 	home := strings.Replace(node.Data, ",", "", 1)
 	// その他医療機関福祉施設等
-	path = "/html/body/div[2]/div[4]/div/div[2]/div/div[2]/table[2]/tbody/tr[4]/td[10]/text()"
+  path = "//div[@id='tmp_contents']/table[2]/tbody/tr[4]/td[10]/text()"
 	node, _ = htmlquery.Query(doc, path)
 	fukushi := strings.Replace(node.Data, ",", "", 1)
 	// 重症
-	path = "/html/body/div[2]/div[4]/div/div[2]/div/div[2]/table[2]/tbody/tr[4]/td[5]/text()"
+  path = "//div[@id='tmp_contents']/table[2]/tbody/tr[4]/td[5]/text()"
 	node, _ = htmlquery.Query(doc, path)
 	jyuu := strings.Replace(node.Data, ",", "", 1)
 	// 死亡
-	path = "/html/body/div[2]/div[4]/div/div[2]/div/div[2]/table[2]/tbody/tr[4]/td[11]/text()"
+  path = "//div[@id='tmp_contents']/table[2]/tbody/tr[4]/td[11]/text()"
 	node, _ = htmlquery.Query(doc, path)
 	shibou := strings.Replace(node.Data, ",", "", 1)
 	// 退院
-	path = "/html/body/div[2]/div[4]/div/div[2]/div/div[2]/table[2]/tbody/tr[4]/td[12]/text()"
+  path = "//div[@id='tmp_contents']/table[2]/tbody/tr[4]/td[12]/text()"
 	node, _ = htmlquery.Query(doc, path)
 	taiin := strings.Replace(node.Data, ",", "", 1)
 
