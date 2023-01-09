4e4cab57c926cfdcba22f6360dec5a053f5a6a5a
mitsugu oyama <mitsugu.oyama@gmail.com>
2023-01-09 15:49:36 +0900

    兵庫県のページの DOM 構造変更に追従

--- gethyogo.go
+++ gethyogo.go
@@ -19,39 +19,24 @@ func main() {
 	d := strings.Replace(strings.Replace(strings.Replace(node.Data, "月", "/", 1), "日24時現在）", "", 1), "（", strconv.Itoa(year)+"/", 1)
 
 	// 宿泊療養
-  path = "//div[@id='tmp_contents']/table[2]/tbody/tr[4]/td[6]/text()"
+  path = "//div[@id='tmp_contents']/table[2]/tbody/tr[4]/td[5]/text()"
 	node, _ = htmlquery.Query(doc, path)
 	syuku := strings.Replace(node.Data, ",", "", 1)
 	// 軽症・中等症
-  path = "//div[@id='tmp_contents']/table[2]/tbody/tr[4]/td[4]/text()"
+  path = "//div[@id='tmp_contents']/table[2]/tbody/tr[4]/td[3]/text()"
 	node, _ = htmlquery.Query(doc, path)
 	kei := strings.Replace(node.Data, ",", "", 1)
-	// 入院・宿泊療養調整等
-  path = "//div[@id='tmp_contents']/table[2]/tbody/tr[4]/td[7]/text()"
-	node, _ = htmlquery.Query(doc, path)
-	cyou := strings.Replace(node.Data, ",", "", 1)
-	// 自宅療養
-  path = "//div[@id='tmp_contents']/table[2]/tbody/tr[4]/td[9]/text()"
-	node, _ = htmlquery.Query(doc, path)
-	home := strings.Replace(node.Data, ",", "", 1)
-	// その他医療機関福祉施設等
-  path = "//div[@id='tmp_contents']/table[2]/tbody/tr[4]/td[10]/text()"
-	node, _ = htmlquery.Query(doc, path)
-	fukushi := strings.Replace(node.Data, ",", "", 1)
 	// 重症
-  path = "//div[@id='tmp_contents']/table[2]/tbody/tr[4]/td[5]/text()"
+  //path = "//div[@id='tmp_contents']/table[2]/tbody/tr[4]/td[4]/text()"
+  path = "//div[@id='tmp_contents']/table[2]/tbody/tr[4]/td[4]/table/tbody/tr[1]/td/text()"
 	node, _ = htmlquery.Query(doc, path)
 	jyuu := strings.Replace(node.Data, ",", "", 1)
 	// 死亡
-  path = "//div[@id='tmp_contents']/table[2]/tbody/tr[4]/td[11]/text()"
+  path = "//div[@id='tmp_contents']/table[2]/tbody/tr[4]/td[6]/text()"
 	node, _ = htmlquery.Query(doc, path)
 	shibou := strings.Replace(node.Data, ",", "", 1)
-	// 退院
-  path = "//div[@id='tmp_contents']/table[2]/tbody/tr[4]/td[12]/text()"
-	node, _ = htmlquery.Query(doc, path)
-	taiin := strings.Replace(node.Data, ",", "", 1)
 
-	fmt.Printf("%v, %v, %v, %v, %v, %v, %v,,, %v, %v\n", d, syuku, kei, cyou, home, fukushi, jyuu, shibou, taiin)
+	fmt.Printf("%v, %v, %v,,,, %v,,, %v,\n", d, syuku, kei, jyuu, shibou)
 
 }
 
